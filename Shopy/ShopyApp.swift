7fc89ff5e26a33399179c99af760d8649a7b866a
Chanuka Mullevidana <chanuka@icanlk.com>
2025-03-03 14:41:33 +0530

    Initial Commit

---
+++ Shopy/ShopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopyApp.swift
+//  Shopy
+//
+//  Created by Chanuka Dilhara on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct ShopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
